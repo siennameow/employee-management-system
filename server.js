bc395e74df89569ac7c75325441ef3473bcf3a5e
Hexuan Li <lihexuan1@gmail.com>
2022-04-30 17:26:16 -0700

    function list to run the app

--- server.js
+++ server.js
@@ -1,23 +1,100 @@
+// Import modules
 const express = require('inquirer');
-// Import and require mysql2
 const mysql = require('mysql2');
-
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+const consoleTables = require("console.table");
+const inquirer = require('inquirer');
 
 // Connect to database
 const db = mysql.createConnection(
     {
       host: 'localhost',
-      // MySQL username,
       user: 'root',
-      // TODO: Add MySQL password here
       password: 'password',
       database: 'emptrack_db'
     },
     console.log(`Connected to the emptrack_db database.`)
   );
+
+// connects to sql server and sql database
+db.connect(function(err){
+    if (err) throw err;
+    start();
+})
+
+// prompts user with list of options to choose from
+function start() {
+    inquirer.prompt ({
+
+    }).then(function(answer){
+
+    })
+}
+
+// view all departments in the database
+function viewDepartment (){
+
+}
+
+//view all roles in the database
+function viewRole () {
+
+}
+
+//view all employees in the database
+function viewEmployee() {
+
+}
+
+// add a department to the database
+function addDepartment () {
+
+}
+
+// add a role to the database
+function addRole () {
+
+}
+
+//add an employee to the database
+function addEmployee () {
+
+}
+
+//update a role in the database
+function updateRole () {
+
+}
+
+//Update employee managers in the database
+function updateEmpManager (){
+
+}
+
+//view employees by manager
+function viewEmpByManager () {
+
+}
+
+//View employees by department
+function viewEmpByDepartment () {
+
+}
+
+//Delete departments
+function deleteDepartment () {
+
+}
+//Delete roles
+function deleteRole(){
+
+}
+
+//Delete employees
+function deleteEmp(){
+
+}
+
+//View the total utilized budget of a department
+function budgetUtilized (){
+    
+}
